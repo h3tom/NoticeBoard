923951cebdf40687cfaaa6d2414e621731419735
Tomasz Hein <hein.tomasz@gmail.com>
2019-09-25 16:19:25 +0200

    Created converter for Category

---
+++ src/main/java/com/noticeBoard/app/converters/NoticeCategoryConverter.java
@@ -0,0 +1,22 @@
+package com.noticeBoard.app.converters;
+
+import com.noticeBoard.app.dto.CategoryDTO;
+import com.noticeBoard.app.model.Category;
+import com.noticeBoard.app.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+
+public class NoticeCategoryConverter implements Converter<String, CategoryDTO> {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public CategoryDTO convert(String source) {
+        Category category = categoryRepository.findById(Long.parseLong(source));
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(category.getId());
+        categoryDTO.setTitle(category.getTitle());
+        return categoryDTO;
+    }
+}
