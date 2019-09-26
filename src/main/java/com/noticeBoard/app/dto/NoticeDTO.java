81d740ef9bdbfb6072733c3cba0944b60e622050
Tomasz Hein <hein.tomasz@gmail.com>
2019-09-26 18:07:43 +0200

    Added NoticeDTO

---
+++ src/main/java/com/noticeBoard/app/dto/NoticeDTO.java
@@ -0,0 +1,82 @@
+package com.noticeBoard.app.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoticeDTO {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime created;
+    private LocalDate endDate;
+    private String image;
+    private String owner;
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<CategoryDTO> categories) {
+        this.categories = categories;
+    }
+}
