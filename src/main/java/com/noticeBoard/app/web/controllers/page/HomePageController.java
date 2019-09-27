8c0d7ed5313031af49461c8c8e2fec539b374def
Tomasz Hein <hein.tomasz@gmail.com>
2019-09-27 20:21:28 +0200

    Updated prepareHomePage method

---
+++ src/main/java/com/noticeBoard/app/web/controllers/page/HomePageController.java
@@ -0,0 +1,32 @@
+package com.noticeBoard.app.web.controllers.page;
+
+import com.noticeBoard.app.services.CategoryService;
+import com.noticeBoard.app.services.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomePageController {
+
+    private NoticeService noticeService;
+    private CategoryService categoryService;
+
+    @Autowired
+    public HomePageController(NoticeService noticeService, CategoryService categoryService) {
+        this.noticeService = noticeService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping({"", "/{id}"})
+    public String prepareHomePage(@PathVariable(required = false) Long id, Model model) {
+        model.addAttribute("allNotices",
+                id == null ? noticeService.getAll() : noticeService.getAllByCategory(id));
+        model.addAttribute("allCategories", categoryService.getAll());
+        return "home";
+    }
+}
