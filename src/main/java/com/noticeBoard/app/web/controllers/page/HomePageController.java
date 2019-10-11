6137fbf7553c4ac57bcc65bec879520649d0053f
Tomasz Hein <hein.tomasz@gmail.com>
2019-10-11 22:05:08 +0200

    Updated HomePageController - added username to view

--- src/main/java/com/noticeBoard/app/web/controllers/page/HomePageController.java
+++ src/main/java/com/noticeBoard/app/web/controllers/page/HomePageController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/home")
 public class HomePageController {
@@ -23,7 +25,10 @@ public class HomePageController {
     }
 
     @GetMapping({"", "/{id}"})
-    public String prepareHomePage(@PathVariable(required = false) Long id, Model model) {
+    public String prepareHomePage(@PathVariable(required = false) Long id, Model model, Principal principal) {
+        if (principal != null) {
+            model.addAttribute("username", principal.getName());
+        }
         model.addAttribute("allNotices",
                 id == null ? noticeService.getAll() : noticeService.getAllByCategory(id));
         model.addAttribute("allCategories", categoryService.getAll());
